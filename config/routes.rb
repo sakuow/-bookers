81a4fedbd06b6681208e8925d1bd9e38255eaf70
sakuow <yusaku1335@gmail.com>
2021-10-03 03:17:23 +0000

    2 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'searches/search'
   get '/home/about' => 'homes#about'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
